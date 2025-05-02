d999cf005f6f8a1fcdefd280e661cd2e3f2552bc
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-05-02 14:25:23 +0700

    config path

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -39,7 +39,7 @@ public class SecurityConfiguration {
                                            CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         http.csrf(c->c.disable())
                 .cors(Customizer.withDefaults())
-                .authorizeHttpRequests(authz -> authz.requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh")
+                .authorizeHttpRequests(authz -> authz.requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/refresh", "/storage/**")
                                 .permitAll()
                 .anyRequest().authenticated()
                 )
